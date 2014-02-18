baf8f9b42dd00f99f8c7ebbc8a64bc1a3155a8a4
Garnet Vaz <garnet.vaz@gmail.com>
2014-02-17 17:20:33 -0800

    completed data download files

--- tests/downloadData.R
+++ tests/downloadData.R
@@ -3,21 +3,26 @@
 ## Datasets include :
 ## Wine quality
 ## Communities and crime
-## Cement quality
+## Concrete strength (TODO)
 
 ## Set download directory name
 cur.dir <- getwd()
-dir.create(file.path(cur.dir,'qtree_datasets'),showWarnings=False)
-data.dir <- paste(cur.dir,'qtree_datasets',sep='')
+dir.create(file.path(cur.dir,'qtree_datasets'),showWarnings=FALSE)
+data.dir <- paste(cur.dir,'/qtree_datasets',sep='')
 
 if(!file.exists(data.dir)) {
     ## Download the wine dataset from http://www3.dsi.uminho.pt/pcortez/wine/
-    wine.url <- url <- 'http://www3.dsi.uminho.pt/pcortez/wine/winequality.zip'
+    wine.url <- 'http://www3.dsi.uminho.pt/pcortez/wine/winequality.zip'
     download.file(url=url,destfile='./qtree_datasets/winequality.zip')
     unzip('qtree_datasets/winequality.zip')
-    whitewine.loc <- './qtree_datasets/winequality/winequality-white.csv'
-    redwine.loc <- './qtree_datasets/winequality/winequality-red.csv'
-    whitewine.data <- read.csv(whitewine.loc,header=)
+    whitewine.loc <- './winequality/winequality-white.csv'
+    whitewine.save.loc <- './qtree_datasets/wine-quality-white.RData'
+    redwine.loc <- './winequality/winequality-red.csv'
+    redwine.save.loc <- './qtree_datasets/wine-quality-red.RData'
+    whitewine.data <- read.csv(whitewine.loc,header=TRUE,sep=';')
+    save(whitewine.data,file=whitewine.save.loc)
+    redwine.data <- read.csv(redwine.loc,header=TRUE,sep=';')
+    save(redwine.data,file=redwine.save.loc)
 
     ## Download the communities and crime dataset from UCI database.
     crime.url <- 'http://archive.ics.uci.edu/ml/machine-learning-databases/communities/communities.data'
@@ -26,9 +31,11 @@ if(!file.exists(data.dir)) {
     ## Eliminate columns with NA
     na.cols <- (sapply(crime.alldata, function(x) sum(is.na(x))) > 0)
     na.cols[1:4] <- TRUE                    # Categorical variables
+    crime.data <- crime.alldata[,!na.cols]
+    names(crime.data) <- paste('X',seq(1L:dim(crime.data)[2]),sep="")
+    crime.save.loc <- './qtree_datasets/crime.RData'
+    save(crime.data,file=crime.save.loc)
 
-    crime.subset <- crime.alldata[,!na.cols]
-    names(crime.subset) <- paste('X',seq(1L:dim(crime.subset)[2]),sep="")
-    save.loc
-    save(crime.subset,file='')
+    ## Clean up
+    file.remove('winequality')
 }
