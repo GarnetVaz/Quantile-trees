b52319e03955757b52f1a1be972ee24caf41c30c
Garnet Vaz <garnet.vaz@gmail.com>
2014-02-18 11:11:02 -0800

    comparison results for white wine w.r.t master

--- tests/wineQuality.R
+++ tests/wineQuality.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env R
-## Time-stamp: <2014-02-17 17:43:22 garnet>
+## Time-stamp: <2014-02-18 11:09:56 garnet>
 ## Simple code to test working of qtree.
 ## For crossvalidation results check the other tests.
 library('tree',quietly=TRUE)
@@ -23,3 +23,19 @@ mod.white.tree <- tree(form, data = whitewine.data,
                        mindev = 0.001, minsize = 4, mincut = 2)
 mod.red.tree <- tree(form, data = redwine.data,
                      mindev = 0.001, minsize = 4, mincut = 2)
+
+lad <- function(y, yhat) {
+    return(sum(abs(y-yhat))/length(yhat))
+}
+## To predict for in sample : mod.red.qtree$frame$yval[mod.red.qtree$where]
+
+## Simple in sample testing to ensure correctness while modifying code.
+insample.white.qtree <- lad(predict(mod.white.qtree,newdata=whitewine.data),whitewine.data$quality)
+insample.red.qtree <- lad(predict(mod.red.qtree,newdata=redwine.data),redwine.data$quality)
+
+cat('In sample white wine MAD is ', insample.white.qtree, '\n')
+cat('In sample red wine MAD is ', insample.red.qtree, '\n')
+
+## Output: Master branch
+## In sample white wine MAD is  0.4897918
+## In sample red wine MAD is  0.3692933
