e5a318a7ba846c5c4ee563b5ed17cf669424da6c
Garnet Vaz <garnet.vaz@gmail.com>
2014-02-17 17:27:57 -0800

    Updated to use the download data script

--- tests/wineQuality.R
+++ tests/wineQuality.R
@@ -1,19 +1,21 @@
 #!/usr/bin/env R
+## Time-stamp: <2014-02-17 17:26:27 garnet>
+## Simple code to test working of qtree.
+## For crossvalidation results check the other tests.
 
 library('qtree',quietly=TRUE)
 
-## Download the wine dataset from http://www3.dsi.uminho.pt/pcortez/wine/
-url <- 'http://www3.dsi.uminho.pt/pcortez/wine/winequality.zip'
-download.file(url=url,destfile='winequality.zip')
-unzip('winequality.zip')
+## Download datasets if not exists.
+source('./downloadData.R')
 
-white <- read.csv('./winequality/winequality-white.csv',sep=';',header=TRUE)
-red <- read.csv('./winequality/winequality-red.csv',sep=';',header=TRUE)
+load('./qtree_datasets/wine-quality-white.RData')
+load('./qtree_datasets/wine-quality-red.RData')
+
 form <- as.formula(quality ~ .)
 
 ## Note that the meaning of minsize and mincut is flipped from the 'tree' package.
-mod.white.qtree <- qtree(form, data = white, mindev = 0.001, minsize = 2, mincut = 4, tau = 0.5)
-mod.red.qtree <- qtree(form, data = red, mindev = 0.001, minsize = 2, mincut = 4, tau = 0.5)
+mod.white.qtree <- qtree(form, data = whitewine.data, mindev = 0.001, minsize = 2, mincut = 4, tau = 0.5)
+mod.red.qtree <- qtree(form, data = redwine.data, mindev = 0.001, minsize = 2, mincut = 4, tau = 0.5)
 
 mod.white.tree <- tree(form, data = white, mindev = 0.001, minsize = 4, mincut = 2)
 mod.red.tree <- tree(form, data = red, mindev = 0.001, minsize = 4, mincut = 2)
