7103d20e960fddbe21135545e8841a925392c5e7
Garnet Vaz <garnet.vaz@gmail.com>
2014-02-19 14:02:22 -0800

    lowered mindev to check error cause

--- tests/wineQuality.R
+++ tests/wineQuality.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env R
-## Time-stamp: <2014-02-18 11:09:56 garnet>
+## Time-stamp: <2014-02-19 14:01:19 garnet>
 ## Simple code to test working of qtree.
 ## For crossvalidation results check the other tests.
 library('tree',quietly=TRUE)
@@ -15,14 +15,14 @@ form <- as.formula(quality ~ .)
 
 ## Note that the meaning of minsize and mincut is flipped from the 'tree' package.
 mod.white.qtree <- qtree(form, data = whitewine.data,
-                         mindev = 0.001, minsize = 2, mincut = 4, tau = 0.5)
+                         mindev = 0.1, minsize = 2, mincut = 4, tau = 0.5)
 mod.red.qtree <- qtree(form, data = redwine.data,
-                       mindev = 0.001, minsize = 2, mincut = 4, tau = 0.5)
+                       mindev = 0.1, minsize = 2, mincut = 4, tau = 0.5)
 
 mod.white.tree <- tree(form, data = whitewine.data,
-                       mindev = 0.001, minsize = 4, mincut = 2)
+                       mindev = 0.1, minsize = 4, mincut = 2)
 mod.red.tree <- tree(form, data = redwine.data,
-                     mindev = 0.001, minsize = 4, mincut = 2)
+                     mindev = 0.1, minsize = 4, mincut = 2)
 
 lad <- function(y, yhat) {
     return(sum(abs(y-yhat))/length(yhat))
